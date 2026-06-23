3c2614b503ab320457babbf36ae1dc390962ff8e
Martin Paul Eve <martin@eve.gd>
2026-06-23 08:35:39 +0100

    6.23.2026

--- minideploy.sh
+++ minideploy.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 cd /home/martin/Documents/Programming/blog
-git add --all .
-git commit -m "$(date +'%-m.%-e.%Y')"
-cd /home/martin/Documents/Programming/blog
 
 bundle exec jekyll build --incremental
-aws s3 sync ./_site/ s3://eve.gd
+
+git add --all .
+git commit -m "$(date +'%-m.%-e.%Y')"
+
+ssh evegd@reclaim -i ~/.ssh/id_ed25519_waldorf.pub /home/evegd/build_site.sh
+
+cd -
+
+#aws s3 sync ./_site/ s3://eve.gd
 #rsync -a ./_site/ torionos:/var/www/html/
 #aws cloudfront create-invalidation --distribution-id E1ARNKVOIBYNHJ --paths "/"
 #aws cloudfront create-invalidation --distribution-id E1ARNKVOIBYNHJ --paths "/index.html"
 #aws cloudfront create-invalidation --distribution-id E1ARNKVOIBYNHJ --paths "/2022/*"
-/home/martin/Documents/Programming/blog/invalidate_cache.sh
+#/home/martin/Documents/Programming/blog/invalidate_cache.sh
 #git push origin master
 
